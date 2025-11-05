38ed573cb2e36ed420ca444a321da3d11cf44593
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-05 16:03:57 +0100

    feat: userName

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,8 +1,20 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
   public static void main(String[] args) {
     String[] invitedList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+  Scanner scanner = new Scanner(System.in);
+
+  // nome utente
+  System.out.println("Scrivi il tuo nome");
+  String userName = scanner.nextLine();
+
+
+
   }
+
 
 }
