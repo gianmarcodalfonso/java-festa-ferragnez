e9c3f288d88ec31570bd4dd5ec300fd37c6177a6
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-05 16:15:54 +0100

    feat: checkGuest logic

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -11,10 +11,20 @@ public class CheckGuest {
   // nome utente
   System.out.println("Scrivi il tuo nome");
   String userName = scanner.nextLine();
+  boolean isInvited = false;
 
-
-
+  for(int i = 0; i < invitedList.length; i++){
+    if(invitedList[i].toLowerCase() == userName.toLowerCase()){
+      isInvited = true;
+    }
   }
 
+  if(isInvited == true){
+    System.out.println("Guest accepted!");
+  }
+  else{
+    System.out.println("Go back home!");
+  }
 
+}
 }
